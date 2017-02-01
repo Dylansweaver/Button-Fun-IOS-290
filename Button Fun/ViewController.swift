fd35b056cd68250f5728747f7fa251516508a170
Dylansweaver <dweaver@a110-m-test.mchenry.edu>
2017-02-01 15:53:04 -0600

    buttonFunIOS290

--- Button Fun/ViewController.swift
+++ Button Fun/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let title = sender.title(for: .selected)!
+        let text = "\(title) button pressed"
+        statusLabel.text = text
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
